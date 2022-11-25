8fb201a8bc82d5bb4356fb60da7801f5c902448d
Milad Rahi <miladrahi@gmail.com>
2022-11-26 02:03:15 +0330

    Add files via upload

---
+++ about.php
@@ -0,0 +1,29 @@
+<?php session_start();
+//$_SESSION['menu'] = "editprofile";
+//$_SESSION['select']='';?>
+<!doctype html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <title>درباره ما</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+
+<body>
+<div id="container">
+<?php include("header.php"); ?>
+
+    <div id="body">
+        <div class="field5" style="font-size: 19px">
+
+                <p><img  src="img/logoabout.png" style="width:80% ;margin: 10% 3% auto auto"></p><br>
+                <p style="margin: 10%;">آدرس : اهواز - پردیس</p>
+                <p>شماره تماس : ۰۹۳۰۰۰۰۰۰۰۰</p><br>
+
+        </div>
+    </div>
+    <?php include("footer.php"); ?>
+</div>
+</body>
+</html>
